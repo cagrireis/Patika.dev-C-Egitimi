ebeb84f380d8b019eb0c11d75c2d87bf8adfc720
Çağrı REİS <65423471+cagrireis@users.noreply.github.com>
2022-10-13 16:44:56 +0300

    soru

---
+++ Ödevler/1.Ödev1.Soru.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome.");
+            Console.Write("Lütfen içinden çift sayıların ayıklanıp ekrana basılacağı kadar sayı gireceğinizden kaç adet sayı gireceğinizi giriniz: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int[] nums = new int[num];
+
+            for (int count = 0; count < num; count ++)
+            {
+                Console.Write("Lütfen {0}. elemanı giriniz: ", (count + 1));
+                nums[count] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            foreach (var element in nums)
+            {
+                if (element % 2 == 0)
+                {
+                    Console.WriteLine(element);
+                }
+            }
+        }
+    }
+}
