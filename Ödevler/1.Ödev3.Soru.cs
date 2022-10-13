b3bb773dbb9262df2e17a32688f839b1a56f14c3
Çağrı REİS <65423471+cagrireis@users.noreply.github.com>
2022-10-13 22:12:56 +0300

    yeni

---
+++ Ödevler/1.Ödev3.Soru.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Girdiğiniz sayı kadar kelime gireceksiniz. Pozitif bir sayı girin: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            string[] words = new string[num];
+
+            for (int ct = 0; ct < num; ct++)
+            {
+                Console.Write("Lütfen {0}. kelimenizi giriniz: ", (ct + 1));
+                words[ct] = Console.ReadLine();
+            }
+
+            Array.Reverse(words);
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
