3f24cbacaaa50a1f2e9689ff4f62560fa08c49f9
Çağrı REİS <65423471+cagrireis@users.noreply.github.com>
2022-10-24 21:36:48 +0300

    yeni

---
+++ Program28.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // OOP giriş:
+            // her şeyin nesne olduğunu savunan bir yazılım şekli
+            // gerçek hayat programlaması gibi.
+            // zamanla olgunlaştı.
+
+            // 3 temel prensibi var
+
+            // 1-) Kapsülleme enkapsülleme
+            // 2-) polymorphism (çok biçimcilik)
+            // 3-) inheritence (kalıtım.)
+
+            // enkapsülleme sınıf içi fieldlarımızı koruma altına alıp public proprtylerle fieldlerimizi private yapıp onları korumamızı sağlıyordu. 
+        }
+    }
+}
